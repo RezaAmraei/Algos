e64e93d022fda532adec2ed78a9532b851534917
Reza Amraei <amraeireza@gmail.com>
2022-06-02 18:16:46 -0700

    non-constructible-change algo completed

--- Easy/non-constructible-change.js
+++ Easy/non-constructible-change.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+function nonConstructibleChange(coins) {
+  // Write your code here.
+  // sort the array
+  coins.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(coins);
+  //init a variable to holder amount of change
+  let currChange = 0;
+  //loop through each coin and check if coin is 1 more than amount of change currently counted
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > currChange + 1) {
+      return currChange + 1;
+    }
+    currChange += coins[i];
+  }
+  return currChange + 1;
+}
